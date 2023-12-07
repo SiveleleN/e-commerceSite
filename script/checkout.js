d0c763a494eb63d12b1bc06a3cede77e7ef2239b
SiveleleN <sivelelenkamane29@gmail.com>
2023-12-07 11:44:11 +0200

    created

--- script/checkout.js
+++ script/checkout.js
@@ -1,3 +1,48 @@
-let shopCart = JSON.parse(localStorage.getItem('checkout'))
+//fetching data from local storage
+document.addEventListener('DOMContentLoaded',() =>{
+    let shopCart = JSON.parse(localStorage.getItem('checkout')) || [] 
+    //I log to fetch data
+    console.log(shopCart);
+    //selecting the table element
+    let myCart = document.querySelector('[data-table]');
+    let continueCheckoutButton = document.getElementById('continueCheckout');
+    let paymentOptionsDiv = document.getElementById('paymentOptions');
+    let submitPaymentButton = document.getElementById('submitPayment');
+    let paymentMethodSelect = document.getElementById('paymentMethod');
+    
+//here I am checking if I have items in my shopping cart
+if (shopCart.length > 0) {
+//doing a table row to display my cart
+myCart.innerHTML = shopCart.map((item, index)=>{
+    return`
+    <tr>
+      <td>${index +1}</td>
+      <td>${item.name}</td>
+      <td>${item.description}</td>
+      <td><img src="${item.img}" height="50px" width = "50px"></td>
+    </tr>`;
+    //I am using join to convert the array of strings in a single string here
+}).join('');
+   //
+} else { 
+    //I will be displayin a name for my shopping cart
+    myCart.innerHTML = '<tr><td colspan ="6">Shoping cart</td></tr>';
+     }
+      // Add event listener for Continue Checkout button
+        continueCheckoutButton.addEventListener('click', () => {
 
-console.log(shopCart);
+         // Additional logic for continuing with checkout
+        // For example, you can show/hide elements or navigate to a checkout page
+        paymentOptionsDiv.style.display = 'block';
+    });
+       // Add event listener for Submit Payment button
+       submitPaymentButton.addEventListener('click', () => {
+        // Get the selected payment method
+        let selectedPaymentMethod = paymentMethodSelect.value;
+        
+        // Additional logic for submitting payment
+        // For example, you can process the payment and show a confirmation message
+        alert(`Payment submitted using ${selectedPaymentMethod}`);
+    });
+
+})
